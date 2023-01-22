34af7fd81cd6b25607be6d22cb9eda0d59844b0d
Mochammad Hairullah <irulgraspama621@gmail.com>
2023-01-22 15:06:43 +0700

    Comments

--- routes/example.route.js
+++ routes/example.route.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 const exampleController = require('./../controllers/api/example.controller')
 
+// METHOD : GET 
 router.get('/', exampleController.index);
-
+// METHOD : GET 
 router.get('/:id', exampleController.show);
-
+// METHOD : POST 
 router.post('/', exampleController.store);
-
+// METHOD : PUT
 router.put('/:id', exampleController.update);
-
+// METHOD : DELETE
 router.delete('/:id', exampleController.destroy);
 
 module.exports = router;
