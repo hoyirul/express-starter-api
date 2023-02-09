9dd587aa1a3addcd9290a8cc3b0f8d8c2feae9a8
Mochammad Hairullah <irulgraspama621@gmail.com>
2023-02-09 19:14:29 +0700

    Update

--- controllers/api/different.controller.js
+++ controllers/api/different.controller.js
@@ -34,10 +34,14 @@ const show = async (req, res) => {
     
 }
 
+const update = async (req, res) => {
+    
+}
+
 const destroy = async (req, res) => {
 
 }
 
 module.exports = {
-    index, show, destroy
+    index, show, update, destroy
 }
